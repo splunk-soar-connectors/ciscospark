bc7b9d0051932097aaba555e062b82f13ac9e6c5
richard.harlan <richard.harlan@phantom.us>
2018-01-10 16:56:53 -0600

    PAPP4062

---
+++ ciscospark_consts.py
@@ -0,0 +1,15 @@
+# --
+#
+# File: ciscospark.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+# Define your constants here
